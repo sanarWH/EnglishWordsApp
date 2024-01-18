b0f3aefad9543011ec4098ae146ec1369458e553
sanarWH <vkhv0702@gmail.com>
2024-01-18 20:53:50 +0300

    The correct answer button works. When you click on a button with incorrect answers, the application crashes

--- app/src/main/java/com/example/englishwordsapp/MainActivity.kt
+++ app/src/main/java/com/example/englishwordsapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.englishwordsapp
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.englishwordsapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +15,118 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.tvQuestionWord.text = "AndroidSprint"
-        binding.tvQuestionWord.setTextColor(Color.MAGENTA)
+        binding.layoutAnswer3.setOnClickListener {
+            markAnswerCorrect()
+        }
+
+        binding.layoutAnswer1.setOnClickListener {
+            markAnswerWrong()
+        }
+    }
+
+    private fun markAnswerWrong() {
+        binding.layoutAnswer1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_wrong
+        )
+
+        binding.tvVariantNumber1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_wrong
+        )
+
+        binding.tvVariantNumber1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_wrong
+            )
+        )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_wrong)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
+    }
+
+    private fun markAnswerCorrect() {
+        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+
+        binding.tvVariantNumber3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_correct
+        )
+
+        binding.tvVariantNumber3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_correct
+            )
+        )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_correct)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
     }
 }
